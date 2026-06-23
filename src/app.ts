edec3a7662ec02f14e8d21c1ffe1fe29f3259b29
MHMIZAN09 <221002550@student.green.edu.bd>
2026-06-23 13:48:45 +0600

    login, register using better auth

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
+import { toNodeHandler } from "better-auth/node";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { envVars } from "./app/config";
+import { auth } from "./app/lib/auth";
 
 const app: Application = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 // Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: envVars.APP_URL,
+    credentials: true, // Allow cookies to be sent with requests
+  }),
+);
 
 // Basic route
 app.get("/", (req: Request, res: Response) => {
