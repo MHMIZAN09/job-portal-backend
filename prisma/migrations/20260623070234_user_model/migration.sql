14a3583870a8a1679649b3b58b8c6a9d570dd479
MHMIZAN09 <221002550@student.green.edu.bd>
2026-06-23 13:03:34 +0600

    users models create

---
+++ prisma/migrations/20260623070234_user_model/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('JOB_SEEKER', 'EMPLOYER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'JOB_SEEKER',
+    "phone" TEXT,
+    "profileImage" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "email_index" ON "users"("email");
